86e35fc36fbe06e746802ae19941c642decd8337
Vineeth Shankar <shankar.vineeth@gmail.com>
2015-01-08 23:57:00 -0500

    Complete

--- checkout.py
+++ checkout.py
@@ -1,6 +1,4 @@
 class Checkout:
-	offer_acount = 3
-	offer_aprice = 130.0
 
 	def __init__(self):
 		self.sum = 0.0
@@ -22,8 +20,6 @@ class Checkout:
 			item_offer = [] # What is the offer
 			
 			for x in f.readlines():
-				print x.split()
-				print '\n'
 				item.append(x.split()[0])
 				cost.append(float(x.split()[1]))
 				item_count.append(0) # Initializing counts for each item
@@ -33,17 +29,15 @@ class Checkout:
 			self.d = dict(zip(item,cost))
 			self.dcount = dict(zip(item,item_count))
 			self.doffer = dict(zip(item_special, item_offer))
-			print self.doffer['B'][1]
-			print self.dcount
+			
 	def scan(self, s):
 		self.dcount[s] += 1
-		if s in self.doffer.keys() and self.dcount[s]%self.doffer[s][0] == 0: # condition for offer qualification
+# condition for offer qualification
+		if s in self.doffer.keys() and self.dcount[s]%self.doffer[s][0] == 0:
 				self.sum += self.doffer[s][1] - (self.doffer[s][0]-1)*self.d[s]
-#			self.sum =  (self.dcount[s]%self.offer_acount)*self.d[s] +(self.dcount[s]/self.offer_acount)*self.offer_aprice
 		else:
 			self.sum += self.d[s]
-		print 'total = '
-		return self.sum
+		print 'total = ',self.sum
 		print '\n'
 		
 	def total(self):
--- test.py
+++ test.py
@@ -2,9 +2,11 @@ from checkout import Checkout
 co1 = Checkout()
 co1.new('pricing_rules.txt')
 
-print co1.scan('A')
-print co1.scan('B')
-print co1.scan('A')
-print co1.scan('A')
+co1.scan('A')
+co1.scan('B')
+co1.scan('A')
+co1.scan('A')
+co1.scan('B')
+co1.scan('C')
 
 print 'The bill is ', co1.total()
